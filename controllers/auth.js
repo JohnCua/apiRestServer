594a8a7a191db7123d7d833bd7999c9735badbb1
johncua <cuatumax@gmail.com>
2022-08-04 03:03:22 -0600

    Google sing added

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const bcryptjs = require('bcryptjs');
-const { response } = require('express');
+const { response, json } = require('express');
 const { generarJWT } = require('../helpers/generar-jwt');
+const { googleVerify } = require('../helpers/google-verify');
 const Usuario = require('../models/usuario');
 
 
@@ -51,6 +52,64 @@ const login  = async (req, res = response ) => {
     }
 }
 
+
+const googleSingIn = async( req, res = response ) =>{
+
+    const { id_token } = req.body;
+
+
+    try {
+
+        const { nombre, img, correo} = await googleVerify(id_token);
+
+        // 
+        let usuario = await Usuario.findOne({correo});
+
+        if( !usuario ) {
+            //Crear usuario
+
+            const data = {
+                nombre,
+                correo,
+                password: ':P',
+                img,
+                google: true
+            };
+
+            usuario = new Usuario(data);
+
+            await usuario.save();
+        }
+
+        //si el usuario esta bloqueado
+        if( !usuario.estado ) {
+            return res.status(401).json({
+                msg: 'Hable con el administrador, usuario bloqueado'
+            })
+        }
+
+         // generar el jwt
+
+         const token = await generarJWT( usuario.id );
+
+        
+        res.json({
+           usuario,
+           token
+        })
+
+    } catch (error) {
+
+        res.status(400).json({
+            ok:false,
+            msg: 'El token  no se pudo verificar',
+            error
+        })
+    }
+
+}
+
 module.exports = {
-    login
+    login,
+    googleSingIn
 }
